d1c3867acedf3db2ae08f6b0d9e6e242a7b39989
SharyZ <onshariiit@gmail.com>
2022-04-07 21:04:18 +0500

    change create_user functionality

--- accounts/managers.py
+++ accounts/managers.py
@@ -2,17 +2,20 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, first_name, last_name, username, email, password, **extra_fields):
-        user = self.model(first_name=first_name, last_name=last_name,
-                          username=username, email=email, **extra_fields)
+    def create_user(self, first_name, last_name, username, email, password):
+        user = self.model(first_name=first_name,
+                          last_name=last_name, username=username, email=email)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+    def create_superuser(self, first_name, last_name, username, email, password):
+        user = self.create_user(first_name=first_name, last_name=last_name,
+                                username=username, email=email, password=password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-        return self.create_user(email, password, **extra_fields)
+        return user
