caacb33ee5121705c5244ebf3f90e8fb24ef75a2
SharyZ <onshariiit@gmail.com>
2022-04-08 23:36:25 +0500

    list categories and retrieve by slug

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Category
 from .serializers import CategorySerializer
@@ -7,5 +10,17 @@ from .serializers import CategorySerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, slug=None):
+        queryset = Category.objects.all()
+        category = get_object_or_404(queryset, slug=slug)
+        serializer = CategorySerializer(category)
+
+        return Response(serializer.data)
